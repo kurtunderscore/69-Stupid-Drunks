0d006ef690cd79604f08c2bfccddc316cfc7082d
kurtunderscore <2005591+kurtunderscore@users.noreply.github.com>
2019-08-19 18:06:24 -0400

    Create drunk.py

---
+++ drunk.py
@@ -0,0 +1,29 @@
+word = "stupid collective"
+
+for straightedge_num in range (69, 0, -1):
+
+	print(straightedge_num, word, "of alcoholics on the wall.")
+
+	print(straightedge_num, word, "of drunks.")
+
+	print("Don't drink it")
+
+	print("Do not even share it around.")
+
+	print("Do not even get a DUI!")
+
+	if straightedge_num == 1:
+
+		print("No more ghetto drunks on the wall.")
+
+	else:
+
+	    new_num = straightedge_num - 1
+
+	    if new_num == 1:
+
+	    	word = "flock"
+
+	print(new_num, word, "of ghetto drunks on the wall.")
+
+print()
